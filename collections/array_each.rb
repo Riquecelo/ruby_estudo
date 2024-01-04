81b20c89b1431eca62172667f24a13ef8a921798
Marcelo Santos <marcelo.has@hotmail.com>
2024-01-04 11:29:02 -0300

    each com array e hash

---
+++ collections/array_each.rb
@@ -0,0 +1,9 @@
+nomes = ['João', 'Manuel', 'José']
+
+nome = 'Leandro'
+
+nomes.each do |nome|
+    puts nome + 'zinho'
+end
+
+puts nome
---
+++ collections/hash_each.rb
@@ -0,0 +1,6 @@
+aulas = {'Aula 1' => 'liberado', 'Aula 2' => 'liberado', 'Aula 3' => 'em breve'}
+
+aulas.each do |key, value|
+    puts "#{key} #{value}"
+    
+end
