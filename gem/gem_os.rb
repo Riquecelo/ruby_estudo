f2e85f37e5f13159ac8d0a304e4e66d503a43e0f
Marcelo Santos <marcelo.has@hotmail.com>
2024-01-06 17:23:08 -0300

    trabalhando com gems

---
+++ gem/gem_os.rb
@@ -0,0 +1,17 @@
+# gem install os
+# gem uninstall os
+require 'os'
+
+def my_os
+    if OS.windows?
+        "Windows"
+    elsif OS.linux?
+        "Linux"
+    elsif OS.mac?
+        "Mac"
+    else
+        "OS não identificado."
+    end
+end
+
+puts "Meu PC possui #{OS.cpu_count} cores, é #{OS.bits} bits e o sistema operacional é #{my_os}"
